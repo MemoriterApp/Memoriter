5195c057f20c7323dfe6ab66253d17da8c1937a1
Johan Trieloff <johan@trieloff.net>
2022-03-23 13:12:50 +0100

    added title and main square

--- memoriter-react/src/App.js
+++ memoriter-react/src/App.js
@@ -2,7 +2,10 @@
 function App() {
   return (
     <body className="body">
-  
+      <header>
+        <h1 className="page_title">Home</h1>
+      </header>
+      <div className="rechteck"></div>
     </body>
   );
 }
