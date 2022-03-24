971b463921d8eba74d088bdf404f43281c5ff864
Johan Trieloff <johan@trieloff.net>
2022-03-24 12:24:32 +0100

    logo

--- memoriter-react/src/App.js
+++ memoriter-react/src/App.js
@@ -4,6 +4,7 @@ function App() {
     <body className="body">
       <header>
         <h1 className="page_title">Home</h1>
+        <img id="Logo-oben" src="Logo.png" alt="site-logo"></img>
       </header>
       <div className="rechteck">
         <div className="main-seperator"></div>
