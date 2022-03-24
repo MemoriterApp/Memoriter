c0062006aa54b2d7818173748c8b86a24da029cd
Johan Trieloff <johan@trieloff.net>
2022-03-24 14:42:18 +0100

    richtig das Logo gestyled

--- memoriter-react/src/App.js
+++ memoriter-react/src/App.js
@@ -1,10 +1,11 @@
+import Logo from './Logo.png'
 
 function App() {
   return (
     <body className="body">
       <header>
         <h1 className="page_title">Home</h1>
-        <img id="Logo-oben" src="Logo.png" alt="site-logo"></img>
+        <img className="Logo-oben" src={Logo} alt="site-logo"></img>
       </header>
       <div className="rechteck">
         <div className="main-seperator"></div>
