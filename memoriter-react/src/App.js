441f12c0dc6818649a4e0fee1974aa52baac3720
Johan Trieloff <johan@trieloff.net>
2022-03-30 14:18:50 +0200

    mehr struktur

--- memoriter-react/src/App.js
+++ memoriter-react/src/App.js
@@ -7,10 +7,15 @@ function App() {
         <h1 className="page_title">Home</h1>
         <img className="Logo-oben" src={Logo} alt="site-logo"></img>
       </header>
-      <div className="rechteck">
-        <h2 className="File-Overview">File Overview</h2>
-        <div className="main-seperator"></div>
-      </div>
+      <main>
+        <div className="rechteck">
+          <h2 className="File-Overview">File Overview</h2>
+          <div className="main-seperator"></div>
+        </div>
+      </main>
+      <footer>
+
+      </footer>
     </body>
   );
 }
