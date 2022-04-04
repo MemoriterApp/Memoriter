00b94295aff15df2c6af7a1384ff11bc6f5cea50
TheLlamamarketer <98122020+TheLlamamarketer@users.noreply.github.com>
2022-04-04 16:10:05 +0200

    hoffentlich ist das keine dopplung

--- memoriter-react/src/components/FolderHome.js
+++ memoriter-react/src/components/FolderHome.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Backdrop from './backdrop';
+import { useState } from 'react';
+import HomepageSettingsClick from './FolderSettingsInteraction'
+
 
 const FolderHome = ({ folder }) => {
-const settingsClick = () => {
-    alert('Nein Simon so funktioniert das nicht')
-}
-const test = () => {
-    alert('test')
-}
+    const test = () => {
+        alert('test')
+    }
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    function settingsHandler() {
+        setModalIsOpen(true);
+    }
 
     return (
         <div className='Folder_Body'>
@@ -16,10 +22,14 @@ const test = () => {
             ) : (
                 <button className='Button_Homepage_Text' onClick={test}>New Folder</button>
             )}
-            <div className='Button_Homepage_Settings' onClick={settingsClick}>
+            <div className='Button_Homepage_Settings' onClick={settingsHandler}>
                 <span className='dot'></span>
                 <span className='dot'></span>
                 <span className='dot'></span>
+            </div>
+            <div>
+                {modalIsOpen && <HomepageSettingsClick/>}
+                {modalIsOpen && <Backdrop/>}
             </div>
         </div>
     );
