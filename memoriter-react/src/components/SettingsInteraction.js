7b69b94a2e5c0f6bd0735099bee117417ec441d8
Johan Trieloff <johan@trieloff.net>
2022-04-04 09:46:48 +0200

    formated sub text

--- memoriter-react/src/components/SettingsInteraction.js
+++ memoriter-react/src/components/SettingsInteraction.js
@@ -4,6 +4,14 @@ function SettingsClick() {
     return (
         <div className='settings-overlay'>
             <h1 className='settings-title'>Settings</h1>
+            <div className='settings-sub'>
+                <p>Profile</p>
+                <p>change user name</p>
+                <p>stats</p>
+                <p>change profile picture</p>
+                <p>delete account</p>
+                <p>Sign out</p>
+            </div>
         </div>
     );
 }
