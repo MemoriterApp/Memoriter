e4f8fd7801602c5d60ea6b367ca82967fcbedc32
Johan Trieloff <johan@trieloff.net>
2022-04-05 10:28:50 +0200

    added link to sub page

--- memoriter-react/src/components/FolderHome.js
+++ memoriter-react/src/components/FolderHome.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Backdropfs from './backdropfs';
 import { useState } from 'react';
 import HomepageSettingsClick from './FolderSettingsInteraction'
+import { Link } from 'react-router-dom';
 
 
 const FolderHome = ({ folder }) => {
@@ -19,24 +20,28 @@ const FolderHome = ({ folder }) => {
 
     return (
         <div className='Folder_Body'>
-            <button className='Button_Homepage' onClick={test}></button>
-            {folder.name != '' ? ( 
-                <button className='Button_Homepage_Text' onClick={test}>{folder.name}</button>
-            ) : (
-                <button className='Button_Homepage_Text' onClick={test}>New Folder</button>
-            )}
-            <div className='Button_Homepage_Settings' onClick={settingsHandler}>
-                <span className='dot'></span>
-                <span className='dot'></span>
-                <span className='dot'></span>
-            </div>
-            <div>
-                {modalIsOpen && <HomepageSettingsClick/>}
-                {modalIsOpen && <Backdropfs/>}
-            </div>
-            <div  onClick={backdropClick}>
+            <Link to='/topic'>
+                <button className='Button_Homepage'></button>
+                {folder.name != '' ? ( 
+                    <button className='Button_Homepage_Text'>{folder.name}</button>
+                ) : (
+                    <button className='Button_Homepage_Text'>New Folder</button>
+                )}
+            </Link>    
+                <div className='Button_Homepage_Settings' onClick={settingsHandler}>
+                    <span className='dot'></span>
+                    <span className='dot'></span>
+                    <span className='dot'></span>
+                </div>
+                <div>
+                    {modalIsOpen && <HomepageSettingsClick/>}
                     {modalIsOpen && <Backdropfs/>}
-            </div>
+                </div>
+                <div  onClick={backdropClick}>
+                        {modalIsOpen && <Backdropfs/>}
+                </div>
+
+           
         </div>
     );
 }
