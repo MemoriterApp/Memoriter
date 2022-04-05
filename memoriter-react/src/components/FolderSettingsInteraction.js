ca8652ee7b5b7328893b055b85a38ec7a6deedce
q-Sci <simon.hubert2904@gmail.com>
2022-04-05 16:48:17 +0200

    Added Folder Edit Form

--- memoriter-react/src/components/FolderSettingsInteraction.js
+++ memoriter-react/src/components/FolderSettingsInteraction.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
+import AddFolderForm from './EditFolderForm';
+import Backdrop from './backdrop';
+import EditFolderForm from './EditFolderForm';
 
 const FolderSettingsClick= () => {
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function EditFolderClick() {
+      setModalIsOpen(true);
+  }
+  function backdropClick() {
+      setModalIsOpen(false);
+  }
+
     return (
         <div className='folder-settings-overlay'>
             <div className='folder-settings-sub'>
-                <p>Rename</p>
-                <p>change icon</p>
-                <p>delete</p>
+                <p onClick={EditFolderClick}>Edit</p>
+                <p>Delete</p>
+            </div>
+            <div>
+                {modalIsOpen && <EditFolderForm />}
+            </div>
+            <div  onClick={backdropClick}>
+                {modalIsOpen && <Backdrop/>}
             </div>
         </div>
     );
