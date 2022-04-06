291e1615f8cb0d397f7c0c75ba38b2d159fbf366
Johan Trieloff <johan@trieloff.net>
2022-04-06 21:42:14 +0200

    set up basic firebase stuff

--- memoriter-react/src/index.js
+++ memoriter-react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import firebase from "./utils/firebase";
 
 ReactDOM.render(
   <BrowserRouter>
--- memoriter-react/src/utils/firebase.js
+++ memoriter-react/src/utils/firebase.js
@@ -1,6 +1,13 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import firebase from "firebase";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDBXsAk4E47Z9WyZ6__2WQNPnZi0aoWA4Q",
   authDomain: "memoriter-802b0.firebaseapp.com",
@@ -11,7 +18,9 @@ const firebaseConfig = {
   measurementId: "G-JES49GGH5N"
 };
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export default firebaseConfig;
+export default firebase;
