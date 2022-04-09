886c081fb864e33b1346dfd9569d9e64e568b3dd
Johan Trieloff <johan@trieloff.net>
2022-04-09 19:04:12 +0200

    tested how rendering things from database works

--- memoriter-react/src/pages/test.js
+++ memoriter-react/src/pages/test.js
@@ -1,5 +1,34 @@
+import { firebase } from '../utils/firebase'
+import { useState, useEffect } from 'react';
+import { async } from '@firebase/util';
+import { collection, getDocs } from 'firebase/firestore/lite';
+
+const { db } = firebase;
+
+
 function TestPage() {
-    return <h>This it the Testpage</h>
+
+    const [folders, setFolders] = useState([]);
+    //Link zur Collection 
+    const foldersCollectionRef = collection(db, "folders");
+
+    useEffect(() => {
+        const getFolder = async () => {
+            const allFolders = await getDocs(foldersCollectionRef) //gibt alles aus einer bestimmten Collection aus
+            setFolders(allFolders.docs.map((doc)=>({...doc.data(), id: doc.id })))
+        };
+        
+        getFolder();
+    }, [])
+
+
+    return (
+    <div>
+        {folders.map((folder) => {
+            return <div> <h1>title: {folder.title}</h1> </div>;
+            })}
+     </div>
+    );
 }
 
 export default TestPage;
