f22ca78fb3cde20587870d3edf8a6f6d3bf119c6
q-Sci <simon.hubert2904@gmail.com>
2022-04-12 11:41:36 +0200

    u can create flashcards now

---
+++ memoriter-react/src/components/FlashcardOpen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FlashcardOpen = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default FlashcardOpen;
--- memoriter-react/src/pages/topic.js
+++ memoriter-react/src/pages/topic.js
@@ -3,19 +3,35 @@ import Logo from './Logo.png';
 import SettingsIcon from '../components/SettingsIcon';
 import Footer from '../components/Footer';
 import Flashcard from '../components/Flashcard';
+import AddFlashcardForm from '../components/AddFlashcardForm';
+import Backdrop from '../components/backdrop';
 import { Link } from 'react-router-dom';
 
 function TopicPage() {
 
+    const [modalIsOpenA, setModalIsOpenA] = useState(false);
+
+    function NewFlashcardClick() {
+        setModalIsOpenA(true);
+    }
+    function backdropClick() {
+        setModalIsOpenA(false);
+    }
+
 //Flashcard Data
-    const [ flashcards, setFlashcards ] = useState([
-        {
-            id: 758494,
-            pos: 1,
-            title: 'yay!',
-            content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
-        },
-    ])
+    const [ flashcards, setFlashcards ] = useState([ ])
+
+//Flashcard Position
+    flashcards.sort(function(a, b){return a.pos - b.pos})
+
+//Add Flashcard
+    const addFlashcard = (flashcard) => {
+        const id = Math.floor(Math.random() * 10000) + 1
+        const pos = flashcard.length + 1
+        const newFlashcardC = { id, pos, ...flashcard }
+        setFlashcards([...flashcards, newFlashcardC])
+        setModalIsOpenA(false)
+    }
 
     return (
         <>
@@ -37,6 +53,21 @@ function TopicPage() {
                         <Flashcard key={flashcard.id} flashcard={flashcard} />
                         ))}
                     </>
+
+                    <div className='Flashcard_Body' onClick={NewFlashcardClick}>
+                        <div className='Flashcard_Rechteck'>
+                            <div className='New_Flashcard_Circle'>
+                                <div className='New_Flashcard_Plus_h' />
+                                <div className='New_Flashcard_Plus_v' />
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        {modalIsOpenA && <AddFlashcardForm onAddFlashcard={addFlashcard} />}
+                    </div>
+                    <div  onClick={backdropClick}>
+                        {modalIsOpenA && <Backdrop/>}
+                    </div>
                 </div>
             </div>
 
