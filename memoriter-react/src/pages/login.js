b4c8165cb0870eb4d00f0bdbeff44760034ec4e0
Johan Trieloff <johan@trieloff.net>
2022-04-27 21:21:11 +0200

    added Logo

--- memoriter-react/src/pages/login.js
+++ memoriter-react/src/pages/login.js
@@ -1,10 +1,20 @@
-function loginPage() {
+import React from "react";
+import Logo from './Logo.png';
+
+function LoginPage() {
     return(
         <div>
-            
+            <header>
+                <img className="Logo-oben-login" src={Logo} alt=''/>
+            </header>
+            <body>
+                <div>
+                    
+                </div>
+            </body>
         </div>
     );
 
-};
+}
 
-export default loginPage;
+export default LoginPage;
