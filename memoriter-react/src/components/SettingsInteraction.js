4ad0f85f140f87dcad30cb7ec89b3c0d5b36bf5a
TheLlamamarketer <98122020+TheLlamamarketer@users.noreply.github.com>
2022-05-03 21:27:00 +0200

    settingschanges

--- memoriter-react/src/components/SettingsInteraction.js
+++ memoriter-react/src/components/SettingsInteraction.js
@@ -6,8 +6,6 @@ function SettingsClick() {
             <h1 className='settings-title'>Set&shy;tings</h1>
             <p className='settings-sub'>Pro&shy;file</p>
             <p className='settings-sub'>Stats</p>
-            <p className='settings-sub'>Chan&shy;ge User&shy;name</p>
-            <p className='settings-sub'>Chan&shy;ge Pro&shy;file Pic&shy;ture</p>
             <p  className='settings-sub' style={{color: 'rgb(228, 48, 48)'}}>De&shy;lete Ac&shy;count</p>
             <p className='settings-sub'>Sign Out</p>
         </div>
