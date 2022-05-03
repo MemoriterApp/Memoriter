699b79f792d4475570167253dcfa234132e7b00a
TheLlamamarketer <98122020+TheLlamamarketer@users.noreply.github.com>
2022-05-03 21:33:40 +0200

    sorry settings agian

--- memoriter-react/src/components/SettingsInteraction.js
+++ memoriter-react/src/components/SettingsInteraction.js
@@ -5,7 +5,7 @@ function SettingsClick() {
         <div className='settings-overlay'>
             <h1 className='settings-title'>Set&shy;tings</h1>
             <p className='settings-sub'>Pro&shy;file</p>
-            <p className='settings-sub'>Stats</p>
+            <p className='settings-sub'>change password</p>
             <p  className='settings-sub' style={{color: 'rgb(228, 48, 48)'}}>De&shy;lete Ac&shy;count</p>
             <p className='settings-sub'>Sign Out</p>
         </div>
