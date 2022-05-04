db33f1ba6da1e4ebf09154b67fa5557e445ffe50
Johan Trieloff <johan@trieloff.net>
2022-05-04 20:28:09 +0200

    added Refs

--- memoriter-react/src/components/mailForm.js
+++ memoriter-react/src/components/mailForm.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { useRef } from 'react';
 
 function MailForm() {
+    const emailRef = useRef()
     return(
         <form>
             <div className="Add_Folder_Form_Text" htmlFor="email">Email Adress:</div>
             <p style={{fontSize: '5px'}} />
-            <input className="Add_Folder_Form_Input" type="email" id="email" name="email"
+            <input className="Add_Folder_Form_Input" ref={emailRef} type="email" id="email" name="email"
                 placeholder='Please enter Email Adress...'/>
             <p style={{fontSize: '25px'}} />
         </form>
--- memoriter-react/src/pages/signup.js
+++ memoriter-react/src/pages/signup.js
@@ -3,9 +3,13 @@ import Logo from './Logo.png';
 import Footer from "../components/Footer";
 import MailForm from "../components/mailForm";
 import WithGoogle from "../components/WithGoogle";
+import { useRef } from "react";
 
 
 function SignUpPage(props) {
+    const passwordRef = useRef();
+    const passwordAgainRef = useRef()
+
     return (
         <div>
             <header className='Page_Header'>
@@ -25,14 +29,14 @@ function SignUpPage(props) {
                         <form>
                             <div className="Add_Folder_Form_Text" htmlFor="password">Password:</div>
                             <p style={{fontSize: '5px'}} />
-                            <input className="Add_Folder_Form_Input" type="password" id="password" name="password"
+                            <input className="Add_Folder_Form_Input" ref={passwordRef} type="password" id="password" name="password"
                                 placeholder="Please Enter Password..."/>
                             <p style={{fontSize: '25px'}} />
                         </form>
                         <form>
                             <div className="Add_Folder_Form_Text" htmlFor="password">password again:</div>
                             <p style={{fontSize: '5px'}} />
-                            <input className="Add_Folder_Form_Input" type="password" id="password" name="password"
+                            <input className="Add_Folder_Form_Input" ref={passwordAgainRef} type="password" id="password" name="password"
                                 placeholder="Please Enter Password again..."/>
                             <p style={{fontSize: '25px'}} />
                         </form>
