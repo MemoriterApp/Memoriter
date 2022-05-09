20647ef41f820c2a50a3aa4e6cfec7eba85bd57c
q-Sci <simon.hubert2904@gmail.com>
2022-05-09 16:46:03 +0200

    sign out confirmation

--- memoriter-react/src/components/SettingsInteraction.js
+++ memoriter-react/src/components/SettingsInteraction.js
@@ -3,13 +3,15 @@ import Backdrop from './backdrop';
 import { useState } from 'react';
 import { firebase } from '../utils/firebase';
 import { onAuthStateChanged, signOut } from "firebase/auth";
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function SettingsClick() {
 
     const [profile, openProfile] = useState(false);
 
-    const [user, setUser] = useState({})
+    const [signOutView, openSignOutView] = useState(false);
+
+    const [user, setUser] = useState({});
 
     onAuthStateChanged(firebase.auth, (currentUser) => {
         setUser(currentUser);
@@ -28,7 +30,18 @@ function SettingsClick() {
             <p className='settings-sub' onClick={() => openProfile(true)}>Pro&shy;file</p>
             <p className='settings-sub'>Chan&shy;ge Pass&shy;word</p>
             <p  className='settings-sub' style={{color: 'rgb(228, 48, 48)'}}>De&shy;lete Ac&shy;count</p>
-            <p className='settings-sub' onClick={logOut}>Sign Out</p>
+            <p className='settings-sub' onClick={() => openSignOutView(true)}>Sign Out</p>
+            {signOutView && <div>
+                <div className='Delete_Folder_Confirm'>
+                    <h2 className='Add_folder_Form_Header'>Do you really want to &nbsp;sign out?</h2>
+                    <button className='Delete_Folder_Confirm_Yes' onClick={logOut}>Yes</button>
+                    <div style={{display: 'inline', color: 'transparent', cursor: 'default'}}>====</div>
+                    <button className='Delete_Folder_Confirm_No' onClick={() => openSignOutView(false)}>No</button>
+                    <p style={{fontSize: '10px'}} />
+                </div>
+                <Backdrop onClick={() => openSignOutView(false)} />
+            </div>}
+
             {profile && <div>
                 <div className='Add_Folder_Form_Body'>
                     <h2 className='Add_Folder_Form_Header'>Profile</h2>
