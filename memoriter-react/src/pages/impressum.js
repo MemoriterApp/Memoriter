52592c866b873d498230633508e8f073ae236f29
Johan Trieloff <johan@trieloff.net>
2022-05-17 21:07:10 +0200

    impressums Text

--- memoriter-react/src/pages/impressum.js
+++ memoriter-react/src/pages/impressum.js
@@ -14,7 +14,18 @@ function ImpressumPage() {
                 </div>
             </Link>
             <h1 className='Legal_Header'>Impressum</h1>
-            <p className='Legal_Text'></p>
+            <p className='Legal_Text'>
+            Memoriter ist ein Schülerunternehmen des Bertha-von-Suttner-Gymnasiums, durchgeführt von JUNIOR. 
+            </p>
+            <ul className="Legal_Text" style={{top: '200px'}}>
+                Kontakt:
+                <li>Johan Trieloff</li>
+                <li>Kopernikusstraße 30</li>
+                <li>14482 Potsdam</li>
+                <li>Deutschland</li>
+                <li>Telefon:  +49 (0)221 | 4981-707</li>
+                <li>Email: johan@trieloff.net</li>
+            </ul>
             <Footer/>
         </div>
     );
