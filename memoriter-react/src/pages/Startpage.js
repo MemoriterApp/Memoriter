f1707a16785d2ce164dd39bd2e365d0251b726a2
TheLlamamarketer <98122020+TheLlamamarketer@users.noreply.github.com>
2022-05-17 21:56:44 +0200

    startpage

--- memoriter-react/src/pages/Startpage.js
+++ memoriter-react/src/pages/Startpage.js
@@ -1,9 +1,24 @@
 import Logo from './Logo.png';
+import SettingsIcon from '../components/SettingsIcon';
+import AddFolderForm from '../components/AddFolderForm';
+import Backdrop from '../components/backdrop';
+import Footer from '../components/Footer';
+
 function Startpage() {
     return (
-    <div>
-        <h1 className="titel">Memoriter</h1>
-    </div>
+         <>
+         <header className='Page_Header'>
+           
+            <img className="Logo-startpage" src={Logo} alt="site-logo" />
+        </header>
+        <div className="rechteck">
+            <h2 className="Overview">Overview</h2>
+                <SettingsIcon />
+        </div>
+            <footer>
+                <Footer />
+            </footer>
+        </>
     );
 }
 export default Startpage;
