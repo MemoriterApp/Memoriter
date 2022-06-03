5b2bee9e7c98f05cd5a81e117ffd6de9da5ef3f1
q-Sci <simon.hubert2904@gmail.com>
2022-06-03 18:29:35 +0200

    fixed grammar stuff

--- memoriter-react/src/pages/About.js
+++ memoriter-react/src/pages/About.js
@@ -15,9 +15,9 @@ function About() {
             </Link>
             <h1 className='Legal_Header'>About Us</h1>
             <p className='Legal_Text'>
-               Memoriter is a Project made by made by six students (Alex, Simon, Leo, Nils, Maya and Johan), from potsdam germany. We created the project 
-               as a part of of JUNIOR programme.
-               The JUNIOR programme has helped the founding process of Memoriter and it would not exist in this way without them. 
+               Memoriter is a Project made by made by six students (Alex, Simon, Leo, Nils, Maya and Johan), from Potsdam, Germany. We created the project 
+               as a part of of JUNIOR-Programme.
+               The JUNIOR-Programme has helped the founding process of Memoriter and it would not exist in this way without them. 
                     It helps students learn economics in school, by making them create a student company and let them deal with the logistics of that.
                     <div></div>The collaboration only lasts one year.
                     Soon the year of their influence over us will be over and we will be able to develop Memoriter more freely and without constraints.
