c8fbd0c4ac62911cc9ad431989e475603e396b37
Johan Trieloff <johan@trieloff.net>
2022-06-03 12:53:39 +0200

    changes ...

--- memoriter-react/src/index.js
+++ memoriter-react/src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import {firebase} from "./utils/firebase";
 
-console.log(firebase);
+
 
 window.firebase = firebase;
 
--- memoriter-react/src/pages/About.js
+++ memoriter-react/src/pages/About.js
@@ -14,8 +14,19 @@ function About() {
                 </div>
             </Link>
             <h1 className='Legal_Header'>About Us</h1>
-            <p className='Legal_Text'></p>
-            <img src={Group}></img>
+            <p className='Legal_Text'>
+               Memoriter is a Project made by made by six students (Alex, Simon, Leo, Nils, Maya and Johan), from potsdam germany. We created the project 
+               as a part of of JUNIOR programme.
+               The JUNIOR programme has helped the founding process of Memoriter and it would not exist in this way without them. 
+                    It helps students learn economics in school, by making them create a student company and let them deal with the logistics of that.
+                    <div></div>The collaboration only lasts one year.
+                    Soon the year of their influence over us will be over and we will be able to develop Memoriter more freely and without constraints.
+                We created the project, because we wanted to learn more about the subject and to make it more accessible. Also we found the world to be lacking an all-inclusive tool, which is Memoriter.
+                We hope you enjoy the project and we hope you will use it in your studies.
+
+                
+            </p>
+            {/*<img src={Group}></img>*/}
             <Footer/>
         </div>
     );
