13867f2ab1c91dd86b852d8deaeb32be6987e054
TheLlamamarketer <98122020+TheLlamamarketer@users.noreply.github.com>
2022-06-06 11:05:07 +0200

    ⋣⋔⊉↉ↈ

--- memoriter-react/src/pages/Start.js
+++ memoriter-react/src/pages/Start.js
@@ -68,7 +68,7 @@ function Start() {
                 <article className='QA-flashcard-content'>
                     <p>
                         Thats the best part, it is totally <span className='bold'>free!</span>. We do not have an incentive to make money off of this project. 
-                        We also do not have any server costs at the moment. But nevertheless if you want to support us you can <span className='bold'>donate</span> 
+                        We also do not have any server costs at the moment. But nevertheless if you want to support us you can  <span className='bold'>donate </span> 
                         to our bank account, more info on the 'About Us'.
                     </p>
                 </article>
