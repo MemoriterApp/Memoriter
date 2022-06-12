d0e5e068965bf80e55205064a46c32d12c96c10e
q-Sci <simon.hubert2904@gmail.com>
2022-06-12 11:51:31 +0200

    change

--- memoriter-react/src/pages/login.js
+++ memoriter-react/src/pages/login.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Logo from './Logo.png';
-import WithGoogle from "../components/WithGoogle";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
