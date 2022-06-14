453c7778b928ba00d21441edfeeb75084647dceb
q-Sci <simon.hubert2904@gmail.com>
2022-06-14 20:06:54 +0200

    improved header quicklinks

--- memoriter-react/src/components/product/product-header.js
+++ memoriter-react/src/components/product/product-header.js
@@ -11,7 +11,9 @@ const ProductHeader = () => {
             <img className='product-header-logo' src={logo} alt='memoriter-logo' onClick={() => navigate('/product')}/>
             {/*Quicklinks:*/}
             <div className='product-header-quicklink-box'>
-                <Link className='product-header-quicklink-current' to='/product'>Product</Link>
+                <Link className='product-header-quicklink' to='/product'>
+                    <span className='product-header-quicklink-current'>Product</span>
+                </Link>
                 <Link className='product-header-quicklink' to='/about'>About</Link>
                 <Link className='product-header-quicklink' to='/blog'>Blog</Link>
                 <Link className='product-header-quicklink' to='/download'>Download</Link>
