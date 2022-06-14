307a9a6bd49868995f5b71a30b5099209a606894
q-Sci <simon.hubert2904@gmail.com>
2022-06-14 18:36:54 +0200

    improved header

--- memoriter-react/src/components/product/product-header.js
+++ memoriter-react/src/components/product/product-header.js
@@ -1,5 +1,5 @@
 import logo from '../../images/logo.png';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const ProductHeader = () => {
 
@@ -7,8 +7,16 @@ const ProductHeader = () => {
 
     return (
         <header className='product-header'>
-            {/*if you click the logo, you will be redirected to the product page*/}
+            {/*If you click the logo, you will be redirected to the product page.*/}
             <img className='product-header-logo' src={logo} alt='memoriter-logo' onClick={() => navigate('/product')}/>
+            {/*Quicklinks:*/}
+            <div className='product-header-quicklink-box'>
+                <Link className='product-header-quicklink-current' to='/product'>Product</Link>
+                <Link className='product-header-quicklink' to='/about'>About</Link>
+                <Link className='product-header-quicklink' to='/blog'>Blog</Link>
+                <Link className='product-header-quicklink' to='/download'>Download</Link>
+                <Link className='product-header-quicklink' to='/donate'>Donate</Link>
+            </div>
         </header>
     );
 }
