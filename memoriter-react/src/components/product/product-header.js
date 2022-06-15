ffa90d71f8a3f33f20acade0832f8a1db469b316
q-Sci <simon.hubert2904@gmail.com>
2022-06-15 14:42:37 +0200

    login and signup buttons

--- memoriter-react/src/components/product/product-header.js
+++ memoriter-react/src/components/product/product-header.js
@@ -1,9 +1,12 @@
 import logo from '../../components/Logo.png';
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 const ProductHeader = () => {
 
     const navigate = useNavigate(); //variable for routing, alternative option for links
+
+    const [onHover, setOnHover] = useState('brightness(1)');
 
     return (
         <header className='product-header'>
@@ -19,10 +22,16 @@ const ProductHeader = () => {
                 <Link className='product-header-quicklink' to='/download'>Download</Link>
                 <Link className='product-header-quicklink' to='/donate'>Donate</Link>
             </nav>
+            {/*sign in and register buttons*/}
             <Link className='product-header-sign-in' to='/login'>
                 <span className='product-header-text-gradient'>Sign in</span>
             </Link>
-            <Link className='product-header-register' to='/signup'>Register</Link>
+            <Link className='product-header-register' to='/signup'
+                onMouseEnter={() => setOnHover('brightness(0.75)')} onMouseLeave={() => setOnHover('brightness(1)')}>
+                {/*the onMouseEnter and -Leave is for the fade effect on hover which was not possible in css*/}
+                <div className='product-header-register-background' style={{filter: onHover}}/>
+                <span className='product-header-register-text'>Register</span>
+            </Link>
         </header>
     );
 }
