33be33052a0e589ca6db54178f35f3d23a062cd7
q-Sci <simon.hubert2904@gmail.com>
2022-06-15 09:14:57 +0200

    added login and signup

--- memoriter-react/src/components/product/product-header.js
+++ memoriter-react/src/components/product/product-header.js
@@ -10,15 +10,19 @@ const ProductHeader = () => {
             {/*If you click the logo, you will be redirected to the product page.*/}
             <img className='product-header-logo' src={logo} alt='memoriter-logo' onClick={() => navigate('/product')}/>
             {/*Quicklinks:*/}
-            <div className='product-header-quicklink-box'>
+            <nav className='product-header-quicklink-box'>
                 <Link className='product-header-quicklink' to='/product'>
-                    <span className='product-header-quicklink-current'>Product</span>
+                    <span className='product-header-text-gradient'>Product</span>
                 </Link>
                 <Link className='product-header-quicklink' to='/about'>About</Link>
                 <Link className='product-header-quicklink' to='/blog'>Blog</Link>
                 <Link className='product-header-quicklink' to='/download'>Download</Link>
                 <Link className='product-header-quicklink' to='/donate'>Donate</Link>
-            </div>
+            </nav>
+            <Link className='product-header-sign-in' to='/login'>
+                <span className='product-header-text-gradient'>Sign in</span>
+            </Link>
+            <Link className='product-header-register' to='signup'>Register</Link>
         </header>
     );
 }
