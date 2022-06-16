2c74df5d241e6b95426d9d7992b8679aaf42ff9b
q-Sci <simon.hubert2904@gmail.com>
2022-06-16 22:38:50 +0200

    just the header animations are missing

--- memoriter-react/src/components/product/product-header.js
+++ memoriter-react/src/components/product/product-header.js
@@ -9,6 +9,7 @@ const ProductHeader = ({ currentPage }) => {
     const navigate = useNavigate(); //variable for routing, alternative option for links
 
     const [onHover, setOnHover] = useState('brightness(1)'); //variable for the hover effect for the register button
+    const [onHoverAlt, setOnHoverAlt] = useState('brightness(1)'); //hover effect for alternative mobile register button
 
     const [mobileSidebar, setMobileSidebar] = useState(false); //variable if the mobile nav sidebar is open or not
 
@@ -41,28 +42,39 @@ const ProductHeader = ({ currentPage }) => {
                     {currentPage === 'donate' ? <span className='product-header-text-gradient'>Donate</span> : <span>Donate</span>}
                 </Link>
                 {/*the if else conditions changes the color of the links depending on the current open page*/}
+
+                {/*light and dark mode button*/}
+                <button className='product-header-visual-mode-button'>
+                    <div className='product-header-visual-mode-icon'/>
+                </button>
+
+                {/*change language button*/}
+                <button className='product-header-language-button'>
+                    <img src={languageIcon} alt='language-icon' className='product-header-language-icon'/>
+                </button>
+            
+                {/*sign in and register buttons*/}
+                <Link className='product-header-sign-in' to='/login'>
+                    <span className='product-header-text-gradient'>Sign in</span>
+                </Link>
+                <Link className='product-header-register' to='/signup'
+                    onMouseEnter={() => setOnHover('brightness(0.75)')} onMouseLeave={() => setOnHover('brightness(1)')}>
+                    {/*the onMouseEnter and -Leave is for the fade effect on hover which was not possible in css*/}
+                    <div className='product-header-register-background' style={{filter: onHover}}/>
+                    <span className='product-header-register-text'>Register</span>
+                </Link>
+                <div className='product-header-quicklink-box-space'/> {/*space at the end for scrolling at the nav sidebar*/}
             </nav>
 
-            {/*sign in and register buttons*/}
-            <Link className='product-header-sign-in' to='/login'>
+            {/*alternative sign in and register button for mobile nav sidebar, staying at the default position*/}
+            <Link className='product-header-sign-in-alt' to='/login'>
                 <span className='product-header-text-gradient'>Sign in</span>
             </Link>
-            <Link className='product-header-register' to='/signup'
-                onMouseEnter={() => setOnHover('brightness(0.75)')} onMouseLeave={() => setOnHover('brightness(1)')}>
-                {/*the onMouseEnter and -Leave is for the fade effect on hover which was not possible in css*/}
-                <div className='product-header-register-background' style={{filter: onHover}}/>
+            <Link className='product-header-register-alt' to='/signup'
+                onMouseEnter={() => setOnHoverAlt('brightness(0.75)')} onMouseLeave={() => setOnHoverAlt('brightness(1)')}>
+                <div className='product-header-register-background' style={{filter: onHoverAlt}}/>
                 <span className='product-header-register-text'>Register</span>
             </Link>
-
-            {/*light and dark mode button*/}
-            <button className='product-header-visual-mode-button'>
-                <div className='product-header-visual-mode-icon'/>
-            </button>
-
-            {/*change language button*/}
-            <button className='product-header-language-button'>
-                <img src={languageIcon} alt='language-icon' className='product-header-language-icon'/>
-            </button>
 
         </header>
     );
