aaa52dfe6e18446234ae3cb74274e56f28c15a14
q-Sci <simon.hubert2904@gmail.com>
2022-06-17 08:39:39 +0200

    working head (imported package)

---
+++ memoriter-react/src/components/head.js
@@ -0,0 +1,18 @@
+import { Helmet } from 'react-helmet'; //React-helmet is a package to create working head tags in React.
+
+
+
+const Head = ({ title, description }) => {
+    return (
+        <Helmet> {/*The helmet tag is like a head tag for meta data, the normal head tag does not work in React.*/}
+            <title>Memoriter | {title}</title> {/*title changes with currently open page*/}
+            {/*all the basic meta data:*/}
+            <meta charset='UTF-8'/>
+            <meta name='viewport' content='width=device-width, initial-scale=1'/>
+            <meta name='keywords' content='memoriter, learning, notes, school'/>
+            <meta name='description' content={description}/>
+        </Helmet>
+    );
+}
+
+export default Head;
--- memoriter-react/src/pages/product.js
+++ memoriter-react/src/pages/product.js
@@ -1,10 +1,17 @@
+import Head from '../components/head';
 import ProductHeader from '../components/product/product-header';
 
 const Product = () => {
     return (
         <>
-            {/*The currentPage property defines the highlighted quicklink ath the navigation bar.*/}
-            <ProductHeader currentPage={'product'}/>
+            {/*head*/}
+            <Head 
+                title=''
+                description=''
+            /> {/*title property is for displaying a custom page title, description is for a custom meta description*/}
+
+            {/*header*/}
+            <ProductHeader currentPage='product'/> {/*The currentPage property defines the highlighted quicklink ath the navigation bar.*/}
         </>
     );
 }
