26f6b9b49f3fbeb6ef26d312114d28444db471ee
q-Sci <simon.hubert2904@gmail.com>
2022-06-17 17:55:53 +0200

    small optimizations

--- memoriter-react/src/pages/product.js
+++ memoriter-react/src/pages/product.js
@@ -6,14 +6,12 @@ const Product = () => {
     return (
         <>
             {/*head*/}
-            <Head
-                title='Product'
-                description='memoriter, learning, notes, home'
-            /> {/*title property is for displaying a custom page title, description is for a custom meta description*/}
+            <Head title='Product' description='memoriter, learning, notes, home'/>
+            {/*title property is for displaying a custom page title, description is for a custom meta description*/}
 
             {/*header*/}
             <ProductHeader currentPage='product'/> {/*The currentPage property defines the highlighted quicklink ath the navigation bar.*/}
-        
+
             {/*alert for too small screens*/}
             <WindowSizeAlert/>
         </>
