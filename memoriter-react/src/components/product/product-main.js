08a3abc5f99ff792da25e85e906e0be8fbb183b7
q-Sci <simon.hubert2904@gmail.com>
2022-06-18 17:22:38 +0200

    font styling

--- memoriter-react/src/components/product/product-main.js
+++ memoriter-react/src/components/product/product-main.js
@@ -3,24 +3,26 @@ import placeholderImage from '../../pages/Memoriter_Katze.jpeg';
 
 const ProductMain = () => {
     return (
-        <div style={{top: '100%'}}>
-            <h1>Placeholder Header</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </p>
-            <img src={placeholderImage} alt='placeholder-image'/>
+        <div style={{top: '100%'}}> {/*the main body is divided in three sections with a header, text and an image*/}
 
-            <h1>Placeholder Header</h1>
-            <img src={placeholderImage} alt='placeholder-image'/>
-            <p>
+            <h1 className='product-main-header'>Placeholder Header</h1>
+            <p className='product-main-text'>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </p>
+            <img className='product-main-image' src={placeholderImage} alt='placeholder-screenshot'/>
+
+            <img className='product-main-image' src={placeholderImage} alt='placeholder-screenshot'/>
+            <h1 className='product-main-header'>Placeholder Header</h1>
+            <p className='product-main-text'>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
             </p>
 
-            <h1>Placeholder Header</h1>
-            <p>
+            <h1 className='product-main-header'>Placeholder Header</h1>
+            <p className='product-main-text'>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
             </p>
-            <img src={placeholderImage} alt='placeholder-image'/>
+            <img className='product-main-image' src={placeholderImage} alt='placeholder-screenshot'/>
+
         </div>
     );
 }
