f4fbb1f51c7c6f71b12758839367bf6e6868d013
q-Sci <simon.hubert2904@gmail.com>
2022-06-19 17:19:13 +0200

    some changes

--- memoriter-react/src/components/product/product-banner.js
+++ memoriter-react/src/components/product/product-banner.js
@@ -1,9 +1,11 @@
 import '../../styles/product-banner.css';
+import productBannerWave from '../../images/product-banner-wave.svg';
 
 const ProductBanner = () => {
     return (
         <div className='product-banner'>
-            Product Banner
+            <img className='product-banner-wave' src={productBannerWave} alt='wave'/>
+            <img className='product-banner-wave-transparent' src={productBannerWave} alt='wave-transparent'/>
         </div>
     );
 }
