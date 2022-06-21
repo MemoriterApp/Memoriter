a668c90ee75af0117a494532eecbbaf0a5dbf8c1
TheLlamamarketer <98122020+TheLlamamarketer@users.noreply.github.com>
2022-06-21 20:44:09 +0200

    basically finished design

--- memoriter-react/src/components/SettingsInteraction.js
+++ memoriter-react/src/components/SettingsInteraction.js
@@ -45,23 +45,42 @@ function SettingsClick() {
             </div>}
 
             {profile && <div>
-                <div className='Add_Folder_Form_Body' style={{left:'50%', top:'27%', height:'350px', width:'450px'}}>
-                    <h2 className='Add_Folder_Form_Header'>Profile</h2>
-                    {/*<div className='Settings-profile-text'>User ID:</div>
-                    <div className='Settings-profile-text' style={{color: '#bbb'}}>{user.uid}</div>*/}
-                    <h1 className='Add_Folder_Form_Header' style={{fontSize: '20px'}}>Personal info</h1>
-                    <p style={{fontSize: '15px'}} />
-                    <div className='Settings-profile-text'>Personal email:
+                <div className='Settings-profile-body'>
+                    <h2 className='Add_Folder_Form_Header'style={{fontSize: '30px'}}>Profile</h2>
+                    <div> 
+                        <h1 className='Settings-profile-header' style={{fontSize: '21px', textAlign:'left', margin:'5px'}}>Personal info</h1>
+                        <div className ='Settings-line'></div>
+                        <p style={{fontSize: '10px'}} />
+                        <div> 
+                            <div className='Settings-profile-text'style={{float:'left',margin:'5px',}}>Personal email:</div>
+                            <div className='Settings-profile-text'style={{color: '#bbb',float:'inline-start', margin:'5px'}}>{user.email}</div>
+                            <div className='Settings-profile-text'style={{float:'inline-start', margin:'5px'}}>Edit</div>
+                        </div>
+                        {/*<div className='Settings-profile-text'>User ID:</div>
+                        <div className='Settings-profile-text' style={{color: '#bbb'}}>{user.uid}</div>*/}
+                        {/*<div>
+                            <div className='Settings-profile-text'style={{float:'left',margin:'5px',}}>Username:</div>
+                            <div className='Settings-profile-text'style={{float:'inline-start', margin:'5px'}}>Edit</div>
+                        </div>*/}
+                        {/*Wenn man etwas vor diesem punkt addiert miuss man das <p> direkt hier drunter vergrößern */}
+                        <p style={{fontSize: '70px'}} />
                     </div>
-                    <div className='Settings-profile-text'style={{color: '#bbb'}}>{user.email}</div>
-                    <p style={{fontSize: '15px'}} />
-                    <div className='Settings-profile-text'>change password</div>
-                    <p style={{fontSize: '15px'}} />
-                    <div className='Settings-profile-text' >change email</div>
-                    <p style={{fontSize: '15px'}} />
-                    <div className='Settings-profile-text'style={{color:''}}>delete account</div>
-                    <div className='Settings-profile-text' style={{fontSize: '15px', color:'lightblue'}} >If you delete your account, your data will be gone forever.</div>
+                        <div>
+                        <h1 className='Settings-profile-header' style={{fontSize: '21px', textAlign:'left', margin:'5px'}}>Password</h1>
+                        <div className ='Settings-line'></div>
+                        <p style={{fontSize: '10px'}} />
+                        <div className='Settings-profile-text'style={{ margin:'5px'}}>change password</div>
+                        <p style={{fontSize: '30px'}} />
+                    </div>
+                    <div>
+                        <h1 className='Settings-profile-header' style={{fontSize: '21px', textAlign:'left', margin:'5px'}}>Account</h1>
+                        <div className ='Settings-line'></div>
+                        <p style={{fontSize: '20px'}} />
+                        <div className='Settings-profile-text'style={{color:'#d83232', margin:'5px'}}>delete account</div>
+                        <div className='Settings-profile-text' style={{fontSize: '15px', color:'rgb(88, 167, 172)',float:'left', margin:'5px'}} >If you delete your account, your data will be gone forever!</div>
+                    </div>
                 </div>
+
                 <Backdrop onClick={() => openProfile(false)} />
             </div>}
         </div>
