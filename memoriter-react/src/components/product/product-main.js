76f6a454cd7e786d0a8af41f421189da28957114
q-Sci <simon.hubert2904@gmail.com>
2022-06-25 21:09:08 +0200

    improved layout

--- memoriter-react/src/components/product/product-main.js
+++ memoriter-react/src/components/product/product-main.js
@@ -6,37 +6,37 @@ const ProductMain = () => {
         <div className='product-main' style={{top: '100%'}}> {/*the height style is for displaying the section correctly under the banner*/}
 
             <div className='product-main-container'>
-                <div>
+                <div className='product-main-container-sub'>
                     <h1 className='product-main-header'>Placeholder Header</h1>
                     <p className='product-main-text'>
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
                     </p>
                 </div>
-                <div>
+                <div className='product-main-container-sub-image'>
                     <img className='product-main-image' src={placeholderImage} alt='placeholder-screenshot'/>
                 </div>
             </div>
 
             <div className='product-main-container'>
-                <div>
+                <div className='product-main-container-sub-image'>
                     <img className='product-main-image' src={placeholderImage} alt='placeholder-screenshot'/>
                 </div>
-                <div>
+                <div className='product-main-container-sub'>
                     <h1 className='product-main-header'>Placeholder Header</h1>
                     <p className='product-main-text'>
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
                     </p>
                 </div>
             </div>
 
             <div className='product-main-container'>
-                <div>
+                <div className='product-main-container-sub'>
                     <h1 className='product-main-header'>Placeholder Header</h1>
                     <p className='product-main-text'>
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
                     </p>
                 </div>
-                <div>
+                <div className='product-main-container-sub-image'>
                     <img className='product-main-image' src={placeholderImage} alt='placeholder-screenshot'/>
                 </div>
             </div>
