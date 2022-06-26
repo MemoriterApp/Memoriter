b7fee9d1225254a27ebfef3f5170a747cc6b65b9
q-Sci <simon.hubert2904@gmail.com>
2022-06-26 17:29:43 +0200

    started content

--- memoriter-react/src/components/product/product-main.js
+++ memoriter-react/src/components/product/product-main.js
@@ -10,9 +10,10 @@ const ProductMain = () => {
                     <div className='product-main-container-sub'>
                         <AnimationOnScroll animateIn='product-main-animation-fade-in-right' animateOut='product-main-animation-fade-out-right'>
                         {/*AnimationOnScroll starts an animation when it enters the viewport*/}
-                            <h1 className='product-main-header'>Placeholder Header</h1>
+                            <h1 className='product-main-header'>The all-in-one learning environment</h1>
                             <p className='product-main-text'>
-                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                                Memoriter is a program combining the information storage sytem of a note-taking tool with the practical advantages of a flashcard learning system,
+                                creating the ultimate all-in-one learning environment.
                             </p>
                         </AnimationOnScroll>
                     </div>
