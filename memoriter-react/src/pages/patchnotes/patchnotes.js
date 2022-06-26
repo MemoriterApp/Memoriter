7a61bc08d8caf1e8b2b84d11dbc3c83cdc14e830
Leo Grünwald <leo.alexander@gmx.de>
2022-06-26 18:35:08 +0200

    Feedback and Releasenotes

---
+++ memoriter-react/src/pages/patchnotes/patchnotes.js
@@ -0,0 +1,31 @@
+import "../../index.css"
+import "./patchnotes.css"
+
+function PatchNotes() {
+    return (
+        <>
+            <header>
+            </header>
+            <body>
+                <div className="patchnotes-releases-box">
+                    <h1>Version 2.0</h1>
+                    <ul>
+                        <li>Test TestTest TestTest TestTest TestTest Test</li>
+                        <li>Test TestTest TestTest TestTest TestTest Test</li>
+                        <li>Test TestTest TestTest TestTest TestTest Test</li>
+                    </ul>
+                </div>
+                <div className="patchnotes-releases-box">
+                    <h1>Version 1.0</h1>
+                    <ul>
+                        <li>Test TestTest TestTest TestTest TestTest Test</li>
+                        <li>Test TestTest TestTest TestTest TestTest Test</li>
+                        <li>Test TestTest TestTest TestTest TestTest Test</li>
+                    </ul>
+                </div>
+            </body>
+        </>
+    );
+}
+
+export default PatchNotes;
