d44fa0e0c415ed624fa90f4074ded56f629fe7ae
q-Sci <simon.hubert2904@gmail.com>
2022-06-27 15:56:01 +0200

    started about page

---
+++ memoriter-react/src/components/about/about-main.js
@@ -0,0 +1,18 @@
+const AboutMain = () => {
+    return (
+        <div style={{position: 'absolute', top: '100px'}}>
+
+            <h1>Who We Are</h1>
+
+            <text>
+                <p>
+                    Memoriter was originally created in 2022 by six students from Potsdam, Germany as a simple note-taking and learning tool.
+                    However, with time, the project grow into something bigger.
+                </p>
+            </text>
+
+        </div>
+    );
+}
+
+export default AboutMain;
