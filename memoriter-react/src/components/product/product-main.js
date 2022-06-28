3eb190d1c06bf3d52eb744810994f69deb5f920a
q-Sci <simon.hubert2904@gmail.com>
2022-06-28 17:38:35 +0200

    removed buggy scroll animation stuff

--- memoriter-react/src/components/product/product-main.js
+++ memoriter-react/src/components/product/product-main.js
@@ -1,6 +1,6 @@
 import '../../styles/product/product-main.css';
 import placeholderImage from '../../images/placeholder.jpeg';
-import { AnimationOnScroll } from 'react-animation-on-scroll';
+//import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 const ProductMain = () => {
     return (
@@ -8,14 +8,14 @@ const ProductMain = () => {
 
                 <div className='product-main-container'>
                     <div className='product-main-container-sub'>
-                        <AnimationOnScroll animateIn='product-main-animation-fade-in-right' animateOut='product-main-animation-fade-out-right'>
+                        
                         {/*AnimationOnScroll starts an animation when it enters the viewport*/}
                             <h1 className='product-main-header'>The all-in-one learning environment.</h1>
                             <p className='product-main-text'>
                                 Memoriter is a program combining the information storage capacity of a note-taking tool with the practical advantages of a flashcard learning system,
                                 creating the ultimate all-in-one learning environment.
                             </p>
-                        </AnimationOnScroll>
+
                     </div>
                     <div className='product-main-container-sub-image'>
                             <img className='product-main-image' src={placeholderImage} alt='placeholder'/>
@@ -28,25 +28,25 @@ const ProductMain = () => {
                     <img className='product-main-image' src={placeholderImage} alt='placeholder'/>
                 </div>
                 <div className='product-main-container-sub'>
-                    <AnimationOnScroll animateIn='product-main-animation-fade-in-left' animateOut='product-main-animation-fade-out-left'>
+                    
                     {/*AnimationOnScroll starts an animation when it enters the viewport*/}
                         <h1 className='product-main-header'>Placeholder Header</h1>
                         <p className='product-main-text'>
                             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
                         </p>
-                    </AnimationOnScroll>
+
                 </div>
             </div>
 
             <div className='product-main-container'>
                 <div className='product-main-container-sub'>
-                    <AnimationOnScroll animateIn='product-main-animation-fade-in-right' animateOut='product-main-animation-fade-out-right'>
+                   
                     {/*AnimationOnScroll starts an animation when it enters the viewport*/}
                         <h1 className='product-main-header'>Placeholder Header</h1>
                         <p className='product-main-text'>
                             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
                         </p>
-                    </AnimationOnScroll>
+
                 </div>
                 <div className='product-main-container-sub-image'>
                     <img className='product-main-image' src={placeholderImage} alt='placeholder'/>
@@ -56,5 +56,8 @@ const ProductMain = () => {
         </div>
     );
 }
+
+//<AnimationOnScroll animateIn='product-main-animation-fade-in-right' animateOut='product-main-animation-fade-out-right'>
+//</AnimationOnScroll>
 
 export default ProductMain;
