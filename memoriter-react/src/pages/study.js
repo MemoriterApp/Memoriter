4e6111291e98dc80280af40cbd41a6126f99b9cb
q-Sci <simon.hubert2904@gmail.com>
2022-07-16 12:34:34 +0200

    added some properties and other changes

--- memoriter-react/src/pages/study.js
+++ memoriter-react/src/pages/study.js
@@ -4,8 +4,11 @@ import Backdropfs from '../components/backdropfs';
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { firebase } from '../utils/firebase'
-import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc, query, where } from 'firebase/firestore/lite';
-import Flashcard from '../components/Flashcard';
+import { collection, getDocs, updateDoc, deleteDoc, doc, query, where } from 'firebase/firestore/lite';
+import parse from 'html-react-parser';
+import { Editor, EditorState, RichUtils, convertToRaw } from 'draft-js';
+import { convertFromHTML, convertToHTML } from 'draft-convert';
+import Backdrop from '../components/backdrop';
 const { db } = firebase;
 
 const StudyPage = () => {
@@ -19,7 +22,13 @@ const StudyPage = () => {
     const flashcardsCollectionRef = query(collection(db, "flashcards"), where("syncedFolder", "==", syncedFolderID));
 
     //Flashcard Data
-    const [flashcards, setFlashcards] = useState([])
+    const [flashcards, setFlashcards] = useState([
+        {
+            id: '',
+            title: '0',
+            content: '0'
+        }
+    ])
 
     //Use Effect für Notes
     useEffect(() => {
@@ -33,6 +42,97 @@ const StudyPage = () => {
     }, [])
 
     const [settingsHandlerOpen, setSettingsHandlerOpen] = useState(false);
+    const [modalIsOpenE, setModalIsOpenE] = useState(false);
+
+    const [title, setTitle] = useState(flashcards[0].title)
+
+    const [editorState, setEditorState] = useState(EditorState.createWithContent(convertFromHTML(flashcards[0].content)));
+
+    const content = sessionStorage.getItem('flashcard-content');
+
+        //Editor Functions
+        function handleKeyCommand (command) {
+            const newState = RichUtils.handleKeyCommand(editorState, command);
+            if (newState) {
+                this.onChange(newState);
+                return 'handled';
+            }
+            return 'not-handled';
+        }
+    
+        const StyleButton = (props) => {
+            let onClickButton = (e) => {
+              e.preventDefault();
+              props.onToggle(props.style);
+            };
+            let className = 'text-editor-button';
+              if (props.active) {
+                className += ' text-editor-button-active';
+                };
+            return <div onMouseDown={onClickButton} className={className}
+                        style={{fontWeight: props.label, fontStyle: props.label, textDecoration: props.label}}>
+                            {props.label}
+                    </div>;
+        };
+    
+        const INLINE_STYLES = [
+            { label: "Bold", style: "BOLD", highlight: 'red' },
+            { label: "Italic", style: "ITALIC" },
+            { label: "Underline", style: "UNDERLINE" }
+        ];
+        
+        const InlineStyleControls = (props) => {
+            const currentStyle = editorState.getCurrentInlineStyle();
+            return (
+                <div>
+                    {INLINE_STYLES.map((type) => (
+                    <StyleButton
+                        key={type.label}
+                        label={type.label}
+                        onToggle={props.onToggle}
+                        style={type.style}
+                        active={currentStyle.has(type.style)}
+                    />
+                    ))}
+                </div>
+            );
+        };
+    
+        const BLOCK_TYPES = [
+            { label: "Bulleted List", style: "unordered-list-item" },
+            { label: "Numbered List", style: "ordered-list-item" }
+          ];
+        
+          const BlockStyleControls = (props) => {
+            const selection = editorState.getSelection();
+            const blockType = editorState
+              .getCurrentContent()
+              .getBlockForKey(selection.getStartKey())
+              .getType();
+            return (
+              <div>
+                {BLOCK_TYPES.map((type) => (
+                  <StyleButton
+                    key={type.label}
+                    label={type.label}
+                    onToggle={props.onToggle}
+                    style={type.style}
+                    active={type.style === blockType}
+                  />
+                ))}
+              </div>
+            );
+          };
+    
+        const onInlineClick = (e) => {
+            let newState = RichUtils.toggleInlineStyle(editorState, e);
+            setEditorState(newState);
+        };
+    
+        const onBlockClick = (e) => {
+            let newState = RichUtils.toggleBlockType(editorState, e);
+            setEditorState(newState);
+        };
 
     return (
         <div>
@@ -66,6 +166,10 @@ const StudyPage = () => {
                         <span className='big-dot'/>
                         <span className='big-dot'/>
                     </div>
+
+                    <h1 style={{textAlign: 'center'}}>{flashcards[0].title}</h1>
+                    <text style={{textAlign: flashcards[0].textAlign}}>{parse(flashcards[0].content)}</text>
+
                 </div>
 
                 {settingsHandlerOpen && <div>
@@ -73,7 +177,7 @@ const StudyPage = () => {
                         <div className='folder-settings-sub'>
                             <p>Text Align:<br/>
                                 {<span style={{color: flashcards[0].textAlignColor}}>{flashcards[0].textAlignSymbol}</span>} {flashcards[0].textAlign}</p>
-                            <p><span style={{color: 'rgb(48, 158, 228)'}}>🖋</span> Edit</p>
+                            <p onClick={() => setModalIsOpenE(true)}><span style={{color: 'rgb(48, 158, 228)'}}>🖋</span> Edit</p>
                             <p style={{color: 'rgb(228, 48, 48)'}}>✕ Delete</p>
                         </div>
                     </div>
@@ -83,6 +187,43 @@ const StudyPage = () => {
                             setSettingsHandlerOpen(false);
                         }}/>
                     </div>
+                </div>}
+
+                {modalIsOpenE && <form className='Edit_Flashcard_Open_Body'>
+                    <div>
+                        <h2 className='Add_Flashcard_Form_Header'>Edit Flashcard</h2>
+                        <p style={{fontSize: '30px'}} />
+                        <textarea rows='2' className='Add_Flashcard_Form_Title' placeholder='Flashcard Title...' maxLength='100'
+                            value={flashcards[0].title} onChange={(changeTitle) => setTitle(changeTitle.target.value)} />
+                        <p style={{fontSize: '20px'}} />
+                        
+                        
+                        <div className='Add_Flashcard_Form_Content'>
+                            <BlockStyleControls onToggle={onBlockClick}/>
+                            <InlineStyleControls onToggle={onInlineClick}/>
+                            <div style={{width: '100%', borderTop: '2px solid rgba(112, 112 ,112 ,1)', margin: '10px 0 10px 0'}}/>
+                            <Editor
+                                placeholder='Flashcard Content...'
+                                editorState={editorState}
+                                onChange={(editorState) => {
+                                    const contentState = editorState.getCurrentContent();
+                                    const saveContent = (contentState) => {
+                                        sessionStorage.setItem('flashcard-content-obj', JSON.stringify(convertToRaw(contentState)));
+                                        sessionStorage.setItem('flashcard-content', convertToHTML(contentState));
+                                    };
+                                    saveContent(contentState);
+                                    setEditorState(editorState);
+                                }}
+                                handleKeyCommand={handleKeyCommand}
+                            />
+                        </div>
+                    
+                    </div>
+                    <input className='Add_Flashcard_Form_Submit' type='submit' value='Done' onClick={
+                        () => { /*onEditFlashcard(flashcards[0].id, flashcards[0].title, flashcards[0].content); */}}/>
+                </form>}
+                {modalIsOpenE && <div onClick={() => setModalIsOpenE(false)}>
+                   <Backdrop/> 
                 </div>}
 
                 <Footer/>
