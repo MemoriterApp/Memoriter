ed5af0b8b730463da29afaba9232dbffb9f5db60
q-Sci <simon.hubert2904@gmail.com>
2022-07-21 19:21:31 +0200

    added third party auth buttons

--- memoriter-react/src/components/sign-in/register.main.jsx
+++ memoriter-react/src/components/sign-in/register.main.jsx
@@ -1,4 +1,8 @@
 import '../../styles/sign-in/sign-in-main.css';
+import googleIcon from '../../images/google-icon.svg';
+import appleIcon from '../../images/apple-icon.svg';
+import facebookIcon from '../../images/facebook-icon.svg';
+import githubIcon from '../../images/github-icon.svg';
 import { useState } from 'react';
 
 const RegisterMain = () => {
@@ -10,6 +14,26 @@ const RegisterMain = () => {
             
             <h1 className='sign-in-main-header'>Register</h1>
 
+            {/*buttons for third party authenticationmethods*/}
+            <div className='sign-in-main-third-party'>
+                <button className='sign-in-main-google'>
+                    <img src={googleIcon} alt='google-icon' className='sign-in-main-google-icon'/>
+                </button>
+
+                <button className='sign-in-main-apple'>
+                    <img src={appleIcon} alt='apple-icon' className='sign-in-main-apple-icon'/>
+                </button>
+
+                <button className='sign-in-main-facebook'>
+                    <img src={facebookIcon} alt='facebook-icon' className='sign-in-main-facebook-icon'/>
+                </button>
+
+                <button className='sign-in-main-github'>
+                    <img src={githubIcon} alt='github-icon' className='sign-in-main-github-icon'/>
+                </button>
+            </div>
+
+            {/*sign up with email form*/}
             <form>
 
                 <input className='sign-in-main-input' type='email' placeholder='Email Adress'/>
