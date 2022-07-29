a901f5571a6766ca5fdba9503074a300c861e826
q-Sci <simon.hubert2904@gmail.com>
2022-07-29 18:02:56 +0200

    added multi line question text support

--- memoriter-react/src/components/faq-question.jsx
+++ memoriter-react/src/components/faq-question.jsx
@@ -3,22 +3,27 @@ import { useState, useEffect, useRef } from 'react';
 
 const FaqQuestion = ({ question, onOpenQuestion }) => {
 
-    const answer = useRef(null); //reference to the answer text
+    const questionText = useRef(null); //reference to the question text
+    const answerText = useRef(null); //reference to the answer text
 
+    const [questionHeight, setQuestionHeight] = useState(32); //gets height of the question text
     const [answerHeight, setAnswerHeight] = useState(0); //gets height of the answer text
 
     useEffect(() => { //useEffect is needed to fix an issue where the value cannot is read before the component renderes, resulted in an error
-        setAnswerHeight(answer.current.clientHeight); //defines the height
+        setQuestionHeight(questionText.current.clientHeight); //defines the question text height
+        setAnswerHeight(answerText.current.clientHeight); //defines the answer text height
     }, []);
 
-    const [open, setOpen] = useState('20px'); //height of container
+    const [open, setOpen] = useState(`calc(${questionHeight}px - 12px)`); //height of container
+    const [openAnimation, setOpenAnimation] = useState(''); //transition for container height (fixes auto transition on page load)
     const [answerOpen, setAnswerOpen] = useState('0'); //answer text vivibility
     const [arrowRotation, setarrowRotation] = useState(''); //rotation of the arrow on the right
     const [arrowOpacity, setArrowOpacity] = useState('1'); //opacity of the arrow on the right
 
     if (question.isOpen === true) { //if a question is opened the styles are changing
-        if (open === '20px') { //second condition prevents infinite loop
-            setOpen(`calc(${answerHeight}px + 42px)`); //height depends on answer text length
+        if (open === `calc(${questionHeight}px - 12px)`) { //second condition prevents infinite loop
+            setOpen(`calc(${questionHeight}px + ${answerHeight}px + 10px)`); //height depends on answer text length
+            setOpenAnimation('height 400ms, background-color 200ms'); //adds height transition
             setAnswerOpen('1');
 
             setArrowOpacity('0');
@@ -29,13 +34,15 @@ const FaqQuestion = ({ question, onOpenQuestion }) => {
             }, 400);
         };
     } else { //if a question is closed the styles are changing back
-        if (open !== '20px') { //second condition prevents infinite loop
-            setOpen('20px');
+        if (open !== `calc(${questionHeight}px - 12px)`) { //second condition prevents infinite loop
+            setOpen(`calc(${questionHeight}px - 12px)`); //sets back to default height
             setAnswerOpen('0');
 
             setArrowOpacity('0');
 
             setTimeout(() => { //timeout used for animation, time in ms
+                setOpenAnimation(''); //removes height transition
+
                 setarrowRotation('');
                 setArrowOpacity('1');
             }, 400);
@@ -43,10 +50,10 @@ const FaqQuestion = ({ question, onOpenQuestion }) => {
     };
 
     return (
-        <div className='faq-question' style={{height: open}} onClick={() => onOpenQuestion(question.question)}>
+        <div className='faq-question' style={{height: open, transition: openAnimation}} onClick={() => onOpenQuestion(question.question)}>
 
-            <h3 className='faq-question-question'>{question.question}</h3>
-            <p className='faq-question-answer' ref={answer} style={{opacity: answerOpen}}>{question.answer}</p>
+            <h3 className='faq-question-question' ref={questionText}>{question.question}</h3>
+            <p className='faq-question-answer' ref={answerText} style={{opacity: answerOpen}}>{question.answer}</p>
 
             <div className='faq-question-open' style={{transform: arrowRotation, opacity: arrowOpacity}}/> {/*arrow at the top right*/}
 
