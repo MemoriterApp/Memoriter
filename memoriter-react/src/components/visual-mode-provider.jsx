0f35b448cbb8dfbf738678a9a2a9c2e8af76fe0b
q-Sci <simon.hubert2904@gmail.com>
2022-09-05 08:55:29 +0200

    more light mode

--- memoriter-react/src/components/visual-mode-provider.jsx
+++ memoriter-react/src/components/visual-mode-provider.jsx
@@ -6,7 +6,7 @@ const VisualModeProvider = ({ children }) => {
 
     const currentMode = localStorage.getItem('visual-mode'); //stored value (localStorage)
 
-    const [visualMode, setVisualMode] = useState('dark'); //visual mode (used as html id to connect to different css ids)
+    const [visualMode, setVisualMode] = useState('light'); //visual mode (used as html id to connect to different css ids)
     const [backgroundColor, setBackgroundColor] = useState(''); //body background color
 
     useEffect(() => { //dynamically changes the page background color and visual mode based on localStorage
@@ -15,7 +15,7 @@ const VisualModeProvider = ({ children }) => {
         };
         
         if (visualMode === 'light') { //checks which mode is active to adjust page background
-            setBackgroundColor('#e4e4e4');
+            setBackgroundColor('#e0e0e0');
         } else {
             setBackgroundColor('#202020');
         };
