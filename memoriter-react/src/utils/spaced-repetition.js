99909b1140b559b73e397d4dba4be5bc0cccb035
q-Sci <simon.hubert2904@gmail.com>
2022-10-16 15:19:25 +0200

    small change

--- memoriter-react/src/utils/spaced-repetition.js
+++ memoriter-react/src/utils/spaced-repetition.js
@@ -5,7 +5,7 @@ import { updateDoc } from 'firebase/firestore/lite';
 
 // function gets the flashcard id answer type, correct answer streak, easiness factor and the last interval
 export const spacedRepetition = (id, type, streak, easiness, interval) => {
-    // five different answer types are possible (0-3)
+    // five different answer types are possible (0-4)
     // 0 is incorrect, 1 is almost correct (with difficulties)
     // 2 is correct and 3 is easy (fast without any difficulties)
 
@@ -14,7 +14,7 @@ export const spacedRepetition = (id, type, streak, easiness, interval) => {
         // new condition to set the interval based on the answer streak
         if (streak === 0) { // answer streak is 0
             interval = 1; // sets interval to 1
-        } else if (streak === 1) {              // answer streak is 1
+        } else if (streak === 1) { // answer streak is 1
             interval = 2; // sets interval to 2 (flashcard will show up again in three days)
         } else if (streak === 2) { // answer streak is 2
             interval = 6; // sets interval to 2 (flashcard will show up again in one week)
@@ -26,7 +26,7 @@ export const spacedRepetition = (id, type, streak, easiness, interval) => {
         date.setDate(date.getDate() + interval + type - 1); // updates date (for the flashcard to showagain) by the last interval
         streak++; // increments answer streak
         
-        easiness = easiness + (0.2 - (3 - type) * (0.08 + (3 - type) * 0.02)); // new easiness factor
+        easiness = easiness + (0.1 - (4 - type) * (0.08 + (4 - type) * 0.02)); // new easiness factor
         if (easiness < 1.2) { // condition for very low easiness
             easiness = 1.2; // changes easiness to a fixed value if it is to low
         };
