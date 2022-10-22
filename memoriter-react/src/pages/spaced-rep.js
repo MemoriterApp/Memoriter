b427609ad4e537590f0903a2930c06c66a754999
TheLlamamarketer <98122020+TheLlamamarketer@users.noreply.github.com>
2022-10-22 19:32:33 +0200

    started changing spaced rep page

--- memoriter-react/src/pages/spaced-rep.js
+++ memoriter-react/src/pages/spaced-rep.js
@@ -1,7 +1,9 @@
 import React from "react";
+import '../css/spaced-rep.css';
 import Footer from "../components/Footer";
 import Logo from '../images/memoriter-logo.svg';
 import { Link } from 'react-router-dom';
+
 
 
 function SpacedRepMode() {
@@ -25,7 +27,31 @@ function SpacedRepMode() {
                     </div>
             </Link> 
             <main>
-                
+                <button className='spaced-rep-button'
+                    style={{ top: '70%', left: '10%'}}
+                    onClick>
+                    ?
+                </button>
+                <button className='spaced-rep-button'
+                    style={{ top: '70%',left: '30%'}}
+                    onClick>
+                    easy 
+                </button>
+                <button className='spaced-rep-button'
+                    style={{ top: '70%',left: '50%'}}
+                    onClick>
+                    correct
+                </button>
+                <button className='spaced-rep-button'
+                    style={{ top: '70%',left: '70%'}}
+                    onClick>
+                    almost correct
+                </button>
+                <button className='spaced-rep-button'
+                    style={{ top: '70%',left: '90%'}}
+                    onClick>
+                    incorrect
+                </button>
             </main>
             <footer>
                 <Footer />
