4f075e1bcbf189030ba93fc80135e09e5f274a34
johan-t <johan@trieloff.net>
2022-12-15 22:02:22 +0100

    tinkered a bit with colors of choose study

--- memoriter-react/src/components/study/choose-studymode.js
+++ memoriter-react/src/components/study/choose-studymode.js
@@ -21,7 +21,7 @@ function ChooseStudyMode() {
                         </div>
                         <h2 style={{ textAlign: 'center', fontSize: '24px' }}>Regular flashcards</h2>
                         <p style={{ marginTop: '15px', textAlign: "center" }}>Practice all of your flashcards, with questions and answers.</p>
-                        <button className="preview-type-button" onClick={() => navigate('/study')}>REGULAR FLASHCARDS</button>
+                        <button  style={{backgroundColor:'var(--current-purple)'}} className="preview-type-button" onClick={() => navigate('/study')}>REGULAR FLASHCARDS</button>
                     </div>
 
                     <div className='preview-type-flashcard' style={{boxShadow: '0.25vw 0.75vh 10px var(--color-shadow-flashcard)'}}>
@@ -32,7 +32,7 @@ function ChooseStudyMode() {
                         </div>
                         <h2 style={{ textAlign: 'center', fontSize: '24px' }}>Spaced repetition</h2>
                          <p style={{ marginTop: '15px', textAlign: "center" }}>Practice your flashcards in intervals and remember everything.</p>
-                        <button style={{bottom: '32px'}} className="preview-type-button" onClick={() => navigate('/study-spaced-repetition')}>SPACED REPETITION</button>
+                        <button style={{bottom: '32px', backgroundColor:'var(--current-blue-dark)'}} className="preview-type-button" onClick={() => navigate('/study-spaced-repetition')}>SPACED REPETITION</button>
                     </div>
                 </div>
                 <div style={{position: 'relative', height: '10px', width: '100%'}}/>
