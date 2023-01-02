fa545162da3f8457723430cb7d8d9a6a6b46c027
Johan_Trieloff <johan@trieloff.net>
2023-01-02 18:55:56 +0100

    commit

--- src/pages/study-modes/regular/flashcard/flashcard-study.js
+++ src/pages/study-modes/regular/flashcard/flashcard-study.js
@@ -12,173 +12,173 @@ import { Remarkable } from 'remarkable';
 import './flashcard-study.css';
 
 const FlashcardStudy = ({
-  flashcard,
-  onIncorrect,
-  onCorrect,
-  onEditFlashcard,
-  onDeleteFlashcard,
-  onChangeTextAlign,
+    flashcard,
+    onIncorrect,
+    onCorrect,
+    onEditFlashcard,
+    onDeleteFlashcard,
+    onChangeTextAlign,
 }) => {
-  const markdown = new Remarkable();
+    const markdown = new Remarkable();
 
-  const [showAnswer, setShowAnswer] = useState(false); //state for showing the answer of the card
+    const [showAnswer, setShowAnswer] = useState(false); //state for showing the answer of the card
 
-  //state for modals
-  const [settingsOverlay, setSettingsOverlay] = useState(false);
-  const [backdropOpen, setBackdropOpen] = useState(false);
-  const [modalIsOpenEdit, setModalIsOpenEdit] = useState(false);
-  const [modalIsOpenDelete, setModalIsOpenDelete] = useState(false);
+    //state for modals
+    const [settingsOverlay, setSettingsOverlay] = useState(false);
+    const [backdropOpen, setBackdropOpen] = useState(false);
+    const [modalIsOpenEdit, setModalIsOpenEdit] = useState(false);
+    const [modalIsOpenDelete, setModalIsOpenDelete] = useState(false);
 
-  function deleteFlashcardReq() {
-    setSettingsOverlay(false);
-    setBackdropOpen(true);
-    setModalIsOpenDelete(true);
-  }
+    function deleteFlashcardReq() {
+        setSettingsOverlay(false);
+        setBackdropOpen(true);
+        setModalIsOpenDelete(true);
+    }
 
-  function editFlashcardReq() {
-    setSettingsOverlay(false);
-    setBackdropOpen(true);
-    setModalIsOpenEdit(true);
-  }
+    function editFlashcardReq() {
+        setSettingsOverlay(false);
+        setBackdropOpen(true);
+        setModalIsOpenEdit(true);
+    }
 
-  return (
-    <div>
-      <div className='study-flashcard-box' onClick={() => setShowAnswer(true)}>
-        <div className='study-flashcard-dots' onClick={() => setSettingsOverlay(true)}>
-          <div className='big-dot' />
-          <div className='big-dot' />
-          <div className='big-dot' />
-        </div>
-
-        <h2 style={{ textAlign: 'center' }}>{flashcard.title}</h2>
-
-        {showAnswer && (
-          <div>
-            <article
-              style={{ marginTop: '30px', textAlign: flashcard.textAlign }}
-              dangerouslySetInnerHTML={{ __html: markdown.render(flashcard.content).trimEnd().replace(/(\r\n|\n|\r)/gm, '') }}
-            />{' '}
-            {/*dangerouslySetInnerHTML parses the formatted html text*/}
-          </div>
-        )}
-
-        {settingsOverlay && (
-          <div
-            className='flashcard-settings-overlay'
-            style={{ transform: 'translate(-24px, 16px)' }}
-          >
-            <div className='flashcard-settings-sub'>
-              <p>
-                {flashcard.textAlign === 'left' || (
-                  <img
-                    className='flashcard-settings-overlay-text-align'
-                    src={alignLeft}
-                    alt=''
-                    onClick={() => onChangeTextAlign(flashcard.id, 'left')}
-                  />
-                )}
-                {flashcard.textAlign === 'right' || (
-                  <img
-                    className='flashcard-settings-overlay-text-align'
-                    src={alignRight}
-                    alt=''
-                    onClick={() => onChangeTextAlign(flashcard.id, 'right')}
-                  />
-                )}
-                {flashcard.textAlign === 'center' || (
-                  <img
-                    className='flashcard-settings-overlay-text-align'
-                    src={alignCenter}
-                    alt=''
-                    onClick={() => onChangeTextAlign(flashcard.id, 'center')}
-                  />
-                )}
-                {flashcard.textAlign === 'justify' || (
-                  <img
-                    className='flashcard-settings-overlay-text-align'
-                    src={alignJustify}
-                    alt=''
-                    onClick={() => onChangeTextAlign(flashcard.id, 'justify')}
-                  />
-                )}
-              </p>
-              <p onClick={editFlashcardReq}>
-                <img
-                  className='icon-settings'
-                  src={edit}
-                  alt=''
-                />
-                Edit
-              </p>
-              <p onClick={deleteFlashcardReq} style={{ color: 'var(--current-red)', filter: 'none' }}>
-                <img
-                  className='icon-settings'
-                  src={deleteIcon}
-                  alt=''
-                />
-                Delete
-              </p>
-            </div>
-          </div>
-        )}
-        <div onClick={() => setSettingsOverlay(false)}>{settingsOverlay && <Backdrop />}</div>
-      </div>
-
-      {showAnswer && (
+    return (
         <div>
-          <div className='flex-container'>
-            <button
-              style={{ backgroundColor: 'var(--current-red)', marginRight: '40px' }}
-              className='correct-incorrect-button'
-              onClick={() => {
-                setShowAnswer(false);
-                onIncorrect(flashcard);
-              }}
-            >
+            <div className='study-flashcard-box' onClick={() => setShowAnswer(true)}>
+                <div className='study-flashcard-dots' onClick={() => setSettingsOverlay(true)}>
+                    <div className='big-dot' />
+                    <div className='big-dot' />
+                    <div className='big-dot' />
+                </div>
+
+                <h2 style={{ textAlign: 'center' }}>{flashcard.title}</h2>
+
+                {showAnswer && (
+                    <div>
+                        <article
+                            style={{ marginTop: '30px', textAlign: flashcard.textAlign }}
+                            dangerouslySetInnerHTML={{ __html: markdown.render(flashcard.content).trimEnd().replace(/(\r\n|\n|\r)/gm, '') }}
+                        />{' '}
+                        {/*dangerouslySetInnerHTML parses the formatted html text*/}
+                    </div>
+                )}
+
+                {settingsOverlay && (
+                    <div
+                        className='flashcard-settings-overlay'
+                        style={{ transform: 'translate(-24px, 16px)' }}
+                    >
+                        <div className='flashcard-settings-sub'>
+                            <p>
+                                {flashcard.textAlign === 'left' || (
+                                    <img
+                                        className='flashcard-settings-overlay-text-align'
+                                        src={alignLeft}
+                                        alt=''
+                                        onClick={() => onChangeTextAlign(flashcard.id, 'left')}
+                                    />
+                                )}
+                                {flashcard.textAlign === 'right' || (
+                                    <img
+                                        className='flashcard-settings-overlay-text-align'
+                                        src={alignRight}
+                                        alt=''
+                                        onClick={() => onChangeTextAlign(flashcard.id, 'right')}
+                                    />
+                                )}
+                                {flashcard.textAlign === 'center' || (
+                                    <img
+                                        className='flashcard-settings-overlay-text-align'
+                                        src={alignCenter}
+                                        alt=''
+                                        onClick={() => onChangeTextAlign(flashcard.id, 'center')}
+                                    />
+                                )}
+                                {flashcard.textAlign === 'justify' || (
+                                    <img
+                                        className='flashcard-settings-overlay-text-align'
+                                        src={alignJustify}
+                                        alt=''
+                                        onClick={() => onChangeTextAlign(flashcard.id, 'justify')}
+                                    />
+                                )}
+                            </p>
+                            <p onClick={editFlashcardReq}>
+                                <img
+                                    className='icon-settings'
+                                    src={edit}
+                                    alt=''
+                                />
+                Edit
+                            </p>
+                            <p onClick={deleteFlashcardReq} style={{ color: 'var(--current-red)', filter: 'none' }}>
+                                <img
+                                    className='icon-settings'
+                                    src={deleteIcon}
+                                    alt=''
+                                />
+                Delete
+                            </p>
+                        </div>
+                    </div>
+                )}
+                <div onClick={() => setSettingsOverlay(false)}>{settingsOverlay && <Backdrop />}</div>
+            </div>
+
+            {showAnswer && (
+                <div>
+                    <div className='flex-container'>
+                        <button
+                            style={{ backgroundColor: 'var(--current-red)', marginRight: '40px' }}
+                            className='correct-incorrect-button'
+                            onClick={() => {
+                                setShowAnswer(false);
+                                onIncorrect(flashcard);
+                            }}
+                        >
               Incorrect
-            </button>
-            <button
-              style={{ backgroundColor: 'var(--current-green)' }}
-              className='correct-incorrect-button'
-              onClick={() => {
-                setShowAnswer(false);
-                onCorrect(flashcard.id);
-              }}
-            >
+                        </button>
+                        <button
+                            style={{ backgroundColor: 'var(--current-green)' }}
+                            className='correct-incorrect-button'
+                            onClick={() => {
+                                setShowAnswer(false);
+                                onCorrect(flashcard.id);
+                            }}
+                        >
               Correct
-            </button>
-          </div>
+                        </button>
+                    </div>
+                </div>
+            )}
+
+            {modalIsOpenEdit && <FlashcardForm
+                type='Edit'
+                flashcard={flashcard}
+                onCancel={() => setModalIsOpenEdit(false)}
+                onConfirm={(title, content) => { onEditFlashcard(flashcard.id, title, content); setModalIsOpenEdit(false); setBackdropOpen(false); }}
+            />}
+
+            {modalIsOpenDelete && <Confirm
+                title='Do you really want to delete this flashcard?'
+                onConfirm={() => onDeleteFlashcard(flashcard.id, flashcard.pos)}
+                onCancel={() => {
+                    setModalIsOpenDelete(false);
+                    setBackdropOpen(false);
+                }}
+            />
+            }
+
+            <div
+                onClick={() => {
+                    setBackdropOpen(false);
+                    setModalIsOpenEdit(false);
+                    setModalIsOpenDelete(false);
+                }}
+            >
+                {backdropOpen && <Backdrop />}
+            </div>
         </div>
-      )}
-
-      {modalIsOpenEdit && <FlashcardForm
-        type='Edit'
-        flashcard={flashcard}
-        onCancel={() => setModalIsOpenEdit(false)}
-        onConfirm={(title, content) => { onEditFlashcard(flashcard.id, title, content); setModalIsOpenEdit(false); setBackdropOpen(false); }}
-      />}
-
-      {modalIsOpenDelete && <Confirm
-        title='Do you really want to delete this flashcard?'
-        onConfirm={() => onDeleteFlashcard(flashcard.id, flashcard.pos)}
-        onCancel={() => {
-          setModalIsOpenDelete(false);
-          setBackdropOpen(false);
-        }}
-      />
-      }
-
-      <div
-        onClick={() => {
-          setBackdropOpen(false);
-          setModalIsOpenEdit(false);
-          setModalIsOpenDelete(false);
-        }}
-      >
-        {backdropOpen && <Backdrop />}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default FlashcardStudy;
