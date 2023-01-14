6ca1e9932cc4a83c6c84bbe195dfa8ae8ce152fa
Johan_Trieloff <johan@trieloff.net>
2023-01-14 12:04:19 +0100

    added premium text

--- src/pages/settings/premium-stuff/premium-ad.js
+++ src/pages/settings/premium-stuff/premium-ad.js
@@ -7,16 +7,28 @@ function PremiumAd() {
             <div className='premium-ad-box'>
                 <h1 className='premium-heading'>MEMORITER PREMIUM</h1>
                 <section>
-                    <h2 className='premium-subheading'>🤖 Get access to CheatPT-3 🤖</h2>
                     <article>
-                        <p className='premium-text'>CheatPT-3 is Memoriter's own Discord Bot. It will help you doing your homework. Simply add it to your server, type '/study' followed by your task and the Bot will generate the perfect answer
-                        The best part: it's only 0.99€</p>
+                        <h2 className='premium-subheading'>🤖 Get access to CheatPT-3 🤖</h2>
+                        <p className='premium-text'>
+                            CheatPT-3 is Memoriter's own Discord Bot. It will help you doing your homework. Simply add it to your server, type '/study' 
+                            followed by your task and the Bot will generate the perfect answer.
+                        </p>
                         <a 
                             className='premium-text' 
                             style={{top:'-20px'}}
                             href='https://discord.gg/cGZXHz9avA' 
                         >Try it out and get more info</a>
                     </article>
+                    <article>
+                        <h2 className='premium-subheading'>✍ Auto-complete your flashcards ✍</h2>
+                        <p className='premium-text'>
+                            Memoriter Premium will automatically complete your flashcards. You don't have to type in the answer anymore. 
+                            Just type in the question and the the ai will suggest the answer. <br/>
+                            Simply press 'tab' to accept the suggestion.
+                        </p>
+                    </article>
+                    <p style={{height:'20px'}}/>
+
                     <a href='https://buy.stripe.com/eVa5kLakf9Jk3mM3cc'>
                         <button className='premium-button'>
                             <p style={{position:'relative', bottom:'10px'}}>GET NOW!</p>
