0aa529742b6b195fd96224cb1c2a4c35729d59ee
Johan_Trieloff <johan@trieloff.net>
2023-03-10 12:16:57 +0100

    basic editor done

--- src/pages/topic-stuff/notes/notes.tsx
+++ src/pages/topic-stuff/notes/notes.tsx
@@ -1,14 +1,39 @@
 import './notes.css'
+import EditorJS from '@editorjs/editorjs';
 function Notes() {
-    //all of the cool frontend code goes here
-  return (
-    <>
-    <div className="note-page">
-        <h1 className='note-header'>Note Page</h1>
-        <textarea className='note-textarea' placeholder='Write your notes here...'></textarea>
-    </div>
-    </>
-  );
+
+    const editor = new EditorJS({
+        holder: 'editorjs',
+        tools: {
+          header: {
+            class: require('@editorjs/header'),
+            shortcut: 'CMD+SHIFT+H'
+          },
+          checklist: {
+            class: require('@editorjs/checklist'),
+            shortcut: 'CMD+SHIFT+C'
+          },
+          list: {
+            class: require('@editorjs/list'),
+            shortcut: 'CMD+SHIFT+L'
+          },
+          quote: {
+            class: require('@editorjs/quote'),
+            shortcut: 'CMD+SHIFT+Q'
+          },
+          link: {
+            class: require('@editorjs/link'),
+            shortcut: 'CMD+SHIFT+K'
+          }
+        }
+      });
+
+      return (
+        <div className="note-page">
+          <h1 className='note-header'>Note Page</h1>
+          <div className='note-textarea' id="editorjs"></div>
+        </div>
+      );
 }
 
 export default Notes;
