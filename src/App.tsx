3b2509e4303aec0f3f3ab79bf074b2e7aa876676
q-Sci <simon.hubert2904@gmail.com>
2023-04-03 14:00:25 +0200

    added/changed footer links, removed outdated pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import Start from './pages/start/start';
-import ImpressumPage from './pages/footer-links/impressum/impressum';
-import PrivacyPage from './pages/footer-links/privacy/privacy-policies';
-import PatchNotes from './pages/footer-links/patch-notes/patch-notes';
 
 import LoginPage from './pages/login-stuff/login/login';
 import SignUpPage from './pages/signup/signup';
@@ -34,14 +31,11 @@ const App = () => {
             <ScrollReset> {/*ScrollReset forces scrolling to top on navigation (fixes issue where the page kept beeing scrolled down)*/}
                 <ThemeProvider> {/*ThemeProvider is responsible for the dark and light theme*/}
                     <Routes>
-                        <Route path='/impressum' element={<ImpressumPage />} />
-                        <Route path='/privacy' element={<PrivacyPage />} />
-                        <Route path='/patch-notes' element={<PatchNotes />} />
                         <Route path='/login' element={<LoginPage />} />
                         <Route path='/signup' element={<SignUpPage />} />
                         <Route path='/topic' element={<TopicPage />} />
                         <Route path='/study' element={<StudyPage />} />
-                        <Route path='/study-spaced-repetition' element={<SpacedRepMode />} />
+                        <Route path='/spaced-repetition' element={<SpacedRepMode />} />
                         {user ? (<> {/*some of the active routes are altered if a user is signed in*/}
                             <Route path='/' element={<HomePage />} />
                         </>) : (<>
