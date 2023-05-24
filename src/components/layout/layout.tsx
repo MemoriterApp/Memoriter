87e204f41f5ce06d4e77cf1e2f35843efc599cfa
q-Sci <simon.hubert2904@gmail.com>
2023-05-24 21:28:35 +0200

    sidebar layout stuff

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,10 +1,24 @@
 import Header from './header';
+import Sidebar from './sidebar';
 
 const Layout = ({ folder, children }: { folder: string; children: React.ReactNode }) => {
   return (
     <>
       <Header folder={folder} />
-      <>{children}</>
+      <div style={{ display: 'flex' }}>
+        <Sidebar />
+        <div
+          style={{
+            position: 'absolute',
+            right: '0',
+            top: '0',
+            width: 'calc(100% - 201px)',
+            height: '100%',
+          }}
+        >
+          {children}
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/components/layout/sidebar.tsx
@@ -0,0 +1,11 @@
+import './sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <aside className='sidebar'>
+      <p>Home</p>
+      <p>Pinned Folders</p>
+    </aside>
+  );
+};
+export default Sidebar;
