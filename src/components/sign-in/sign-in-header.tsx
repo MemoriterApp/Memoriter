015960b88223b4501e34995bd3c506a5e2ef1f33
Simon H <97528607+q-Sci@users.noreply.github.com>
2023-07-23 16:00:42 +0200

    \n

--- src/components/sign-in/sign-in-header.tsx
+++ src/components/sign-in/sign-in-header.tsx
@@ -5,6 +5,7 @@ import memoriterLogoBlack from '../../images/memoriter-logo-black.svg';
 
 const SignInHeader = () => {
   const theme: string = useSelector((state: any) => state.theme.value); // current light or dark mode icon based on theme
+  
   return (
     <header className='sign-in-header'>
       {/* If you click the logo, you will be redirected to the product page. */}
